917a1ab57c818ce32642ad730954111bbd99908a
Hiroaki Takahashi <hrktkhsh63784@gmail.com>
2019-03-08 19:47:23 +0900

    Modify voc_label.py

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -4,9 +4,9 @@ import os
 from os import listdir, getcwd
 from os.path import join
 
-sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+sets=[('2007', 'train'), ('2007', 'trainval'), ('2007', 'val')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["person"]
 
 
 def convert(size, box):
@@ -23,8 +23,8 @@ def convert(size, box):
     return (x,y,w,h)
 
 def convert_annotation(year, image_id):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    out_file = open('VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id), 'w')
+    in_file = open('../aushop_fullbody_tx2vx01_20190225/Converted_Root/Annotations/%s.xml'%(image_id))
+    out_file = open('../aushop_fullbody_tx2vx01_20190225/Converted_Root/labels/%s.txt'%(image_id), 'w')
     tree=ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -32,9 +32,9 @@ def convert_annotation(year, image_id):
     h = int(size.find('height').text)
 
     for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
+        occluded = obj.find('occluded').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in classes or int(occluded)==1:
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
@@ -45,15 +45,15 @@ def convert_annotation(year, image_id):
 wd = getcwd()
 
 for year, image_set in sets:
-    if not os.path.exists('VOCdevkit/VOC%s/labels/'%(year)):
-        os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
+    if not os.path.exists('../aushop_fullbody_tx2vx01_20190225/Converted_Root/labels/'):
+        os.makedirs('../aushop_fullbody_tx2vx01_20190225/Converted_Root/labels/')
+    image_ids = open('../aushop_fullbody_tx2vx01_20190225/Converted_Root/ImageSets/Main/%s.txt'%(image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
     for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
+        list_file.write('../aushop_fullbody_tx2vx01_20190225/Converted_Root/JPEGImages/%s.jpg\n'%(image_id))
         convert_annotation(year, image_id)
     list_file.close()
 
-os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
-os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > train.all.txt")
+os.system("cat 2007_train.txt > train.txt")
+os.system("cat 2007_trainval.txt > train.all.txt")
 
