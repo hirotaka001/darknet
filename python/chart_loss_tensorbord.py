505245e185e83dbdc1b35b7d2f0370ee653580a6
ha-takahashi <ha-takahashi@kddi.com>
2019-03-26 13:09:41 +0900

    Add chart_loss_tensorbord.py

---
+++ python/chart_loss_tensorbord.py
@@ -0,0 +1,11 @@
+import tensorboardX as tbx
+
+#出力先のファイル名指定
+writer = tbx.SummaryWriter("../logs/exp-1")
+
+with open("../logs/loss.txt") as f:
+    line = f.readline()
+    num = line.split(',')
+    writer.add_scalar("group/epoch", num[1], num[0])
+
+writer.close()
