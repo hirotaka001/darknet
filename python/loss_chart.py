8a1926d1af4fc39c8e1ecfbd0254c4d8e02889d9
ha-takahashi <ha-takahashi@kddi.com>
2019-03-26 12:16:02 +0900

    Modify loss_chart.py

--- python/loss_chart.py
+++ python/loss_chart.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('../logs/loss.txt', names=['num1', 'num2'])
-plt.plot(range(0, 10), df['num2'], marker="o")
+plt.plot(df['num1'], df['num2'], marker="o")
+plt.savefig('../logs/figure.png')
 plt.show()
